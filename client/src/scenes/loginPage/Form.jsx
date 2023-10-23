229c84fd959a69cda838fd98eae79bca06c44217
Dammymure <dammymure1@gmail.com>
2023-10-23 11:06:20 +0100

    Completed the backend and navbar frontend

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { EditOutlined } from "@mui/icons-material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+
+// TO VALIDATE Form
+const registerSchema = yup.object().shape({
+  firstname: yup.string().required("required"),
+  lastname: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  pasword: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  pasword: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
+
+const Form = () => {
+  const [pageType, setPageType] = useState("login");
+  const { palette } = useTheme();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const isLogin = pageType === "login";
+  const isRegister = pageType === "register";
+
+  const handleFormSubmit = async (values, onsubmitProps) => {};
+
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+      validationSchema={isLogin ? loginSchema : registerSchema}
+    ></Formik>
+  );
+};
+
+export default Form;
